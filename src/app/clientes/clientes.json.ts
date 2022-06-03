1bda691344d73cf4d46a6fd1871585aa20173761
Angel <angelluis.fernandez.morante.contractor@bbva.com>
2022-06-03 09:52:49 +0200

    curso angular

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,39 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {
+    id: 1,
+    nombre: 'Angel',
+    apellido: 'Fernandez',
+    email: 'angel@gmail.com',
+    createAt: '2022-06-01'
+  },
+  {
+    id: 2,
+    nombre: 'Noemi',
+    apellido: 'Molano',
+    email: 'noemi@gmail.com',
+    createAt: '2022-06-03'
+  },
+  {
+    id: 3,
+    nombre: 'Jaime',
+    apellido: 'Lorente',
+    email: 'jaime@gmail.com',
+    createAt: '2012-09-01'
+  },
+  {
+    id: 4,
+    nombre: 'Ibai',
+    apellido: 'Llanos',
+    email: 'ibai@gmail.com',
+    createAt: '2010-08-43'
+  },
+  {
+    id: 5,
+    nombre: 'Cristinini',
+    apellido: 'Garcia',
+    email: 'cristi@gmail.com',
+    createAt: '2000-12-15'
+  }
+];
